f2c4506cc1d534593dcd58d343e7582c76e2a97a
Ragnorok303 <jasontompkins22@gmail.com>
2020-06-14 10:20:54 -0600

    added routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const apiRoutes = require("./api")
+
+router.use('/api', apiRoutes);
+
+router.use(function(req, res){
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+module.exports = router;
